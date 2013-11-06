28b409ae88c5c9ec1e8cbc6e31d456e19ed26fb2
andrew.g.crichton@gmail.com <andrew.g.crichton@gmail.com@0eea08a1-903a-204f-7ded-f54e91f5164d>
2013-11-06 21:53:16 +0000

    Nuke Chris's modifications to net file. Fix checksum. Fully functional telemetry send.
    git-svn-id: https://warg-explorer16-autopilot.googlecode.com/svn/Autopilot.X@57 0eea08a1-903a-204f-7ded-f54e91f5164d

--- net.c
+++ net.c
@@ -36,25 +36,22 @@ struct telem_block *createTelemetryBlock(void) {
 struct telem_block *getDebugTelemetryBlock(void) {
     // If the telemetry block does not exist, create it with
     // alternating ones and zeros (0xAA)
-    UART1_SendString("Repeat");
     if (debugTelemetry == 0) {
         debugTelemetry = createTelemetryBlock();
-        debugTelemetry->millis = (long long) 1;
-        debugTelemetry->lat = (long double) 1;
-        debugTelemetry->lon = (long double) 1;
-        debugTelemetry->pitch = (float) 1;
-        debugTelemetry->roll = (float) 1;;
-        debugTelemetry->yaw = (float) 1;
-        debugTelemetry->pitchRate = (float) 1;
-        debugTelemetry->rollRate = (float) 1;
-        debugTelemetry->yawRate = (float) 1;
-        debugTelemetry->pitch_gain = (float) 1;
-        debugTelemetry->roll_gain = (float) 1;
-        debugTelemetry->yaw_gain = (float) 1;
-        debugTelemetry->pitchSetpoint = (int) 1;
-        debugTelemetry->rollSetpoint = (int) 1;
-        debugTelemetry->yawSetpoint = (int) 1;
-        debugTelemetry->editing_gain = (char)1;
+        unsigned char *debugTelemetryArray = debugTelemetry;
+        int i;
+        for ( i = 0; i < sizeof(struct telem_block); i++) {
+            if ( i % 8 == 0)
+                debugTelemetryArray[i] = 0xAA;
+            else if ( i % 8 == 1)
+                debugTelemetryArray[i] = 0xBB;
+            else if ( i % 8 == 2)
+                debugTelemetryArray[i] = 0xCC;
+            else if ( i % 8 == 3)
+                debugTelemetryArray[i] = 0xDD;
+            else if ( i % 8 == 4)
+                debugTelemetryArray[i] = 0xEE;
+        }
     }
 
     return debugTelemetry;
@@ -64,6 +61,7 @@ struct telem_block *getDebugTelemetryBlock(void) {
 
 void destroyTelemetryBlock(struct telem_block *telem) {
     free(telem);
+    telem = 0;
 }
 
 // Add a telem_block to the outbound telemetry queue
@@ -140,134 +138,80 @@ struct telem_block *popTelemetryBlock(void) {
     return (void *) 0;
 }
 
+//TODO: Implement method to split telemetry data between multiple packets
+
 // generate an api string into the given char array from the telem block
 // Does not check array length!
+unsigned int generateApiHeader(unsigned char *apiString, char dataFrame) {
+    unsigned int apiIndex = 0;
+    unsigned int length = API_HEADER_LENGTH - 3 + sizeof(struct telem_block);
+    unsigned int telemLength = sizeof(struct telem_block);
 
-unsigned int generateApiString(unsigned char *apiString,
-        struct telem_block *telem) {
-    // TODO: No need to re-init this every time.
-    int apiIndex = 0;
-    char dataFrame = 0; //0 for now, we don't care about ACKs
-    int length = 5+4+sizeof(struct telem_block);
-    if (sizeof(struct telem_block) > MAX_PACKET_SIZE){
-        //TODO: Implement method to split telemetry data between multiple packets
-    }
-    apiString[apiIndex] = 0x7E; apiIndex++;
-    apiString[apiIndex] = length & 0xFF00; apiIndex++;
-    apiString[apiIndex] = length & 0x00FF; apiIndex++;
+    // API Mode header
+    apiString[apiIndex++] = 0x7E;
+    // Packet length
+    apiString[apiIndex] = (length & 0xFF00); apiIndex++;
+    apiString[apiIndex] = (length & 0x00FF); apiIndex++; // XXX: Big endian/little endian?
 
-    /*API STRING STARTS HERE*/
-
-    apiString[apiIndex] = TRANSMIT_16BIT; apiIndex++;
-    apiString[apiIndex] = dataFrame; apiIndex++;
-    apiString[apiIndex] = RECEIVER_ADDRESS_MSB; apiIndex++;
-    apiString[apiIndex] = RECEIVER_ADDRESS_LSB; apiIndex++;
-    apiString[apiIndex] = OPTION_BYTE; apiIndex++;
-    // Set up api prefix
-    apiString[apiIndex] = '$';
-    apiIndex++;
-    apiString[apiIndex] = 'S';
-    apiIndex++;
-    apiString[apiIndex] = 'P';
-    apiIndex++;
-    apiString[apiIndex] = ',';
-    apiIndex++;
-
-    // I'M A WIZARD
-    char *telemStart = (char*) &apiString[apiIndex];
-    memcpy(telemStart, telem, sizeof (struct telem_block));
-    apiIndex += sizeof (struct telem_block);
-
-    /*API STRING STARTS ENDS*/
-    //Calculate Checksum
-    //IF OPTIMIIZATION IS REQUIRED, PRECALCULATE THE HEADER CHECKSUM
-    int j = 0;
-    unsigned char checksum = 0;
-    for (j = PACKET_HEADER_LENGTH; j < apiIndex; j++) {
-        checksum += apiString[j] & 0xFF;
-    }
-    apiString[apiIndex++] = 0xFF - checksum;
-
+    // API Identifier
+    apiString[apiIndex++] = TRANSMIT_16BIT;
+    // Data frame
+    apiString[apiIndex++] = dataFrame;
+    // Receiver address
+    apiString[apiIndex++] = RECEIVER_ADDRESS_MSB;
+    apiString[apiIndex++] = RECEIVER_ADDRESS_LSB;
+    // Option byte
+    apiString[apiIndex++] = OPTION_BYTE;
 
     return apiIndex;
-    // TODO: Add additional telemetry block parameters to be sent here
-}
-
-int createPackets(unsigned int stringLength, unsigned char *apiString, unsigned char **packets, unsigned char *packetLength) {
-    int packetIndex = 0;
-    unsigned char dataFrame = 0;
-
-    //If the data is to big to fit in one packet, split it
-    int nPackets = stringLength / DEFAULT_SEND_LIMIT + 1;
-
-    int i = 0;
-    for (i = 0; i < nPackets; i++) {
-        if (INCREMENT_DATA_FRAME) {
-            dataFrame++;
-        }
-        int totalLength = stringLength + API_HEADER_LENGTH;
-        packets[i][packetIndex++] = 0x7E;
-        packets[i][packetIndex++] = totalLength & 0xFF00;
-        packets[i][packetIndex++] = totalLength & 0x00FF;
-        packets[i][packetIndex++] = TRANSMIT_16BIT;
-        packets[i][packetIndex++] = dataFrame;
-        packets[i][packetIndex++] = RECEIVER_ADDRESS_MSB;
-        packets[i][packetIndex++] = RECEIVER_ADDRESS_LSB;
-        packets[i][packetIndex++] = OPTION_BYTE;
-
-        int packetSize = 0;
-        for (packetSize = packetIndex; packetSize < stringLength + packetIndex; packetSize++) {
-            //Add the data in
-            packets[i][packetSize] = apiString[packetSize - packetIndex];
-        }
-        //Checksum calculation
-        //IF OPTIMIIZATION IS REQUIRED, PRECALCULATE THE HEADER CHECKSUM
-        int j = 0;
-        unsigned char checksum = 0;
-        for (j = packetIndex - API_HEADER_LENGTH; j < packetSize; j++) {
-            checksum += packets[i][j] & 0xFF;
-        }
-        packets[i][packetSize++] = 0xFF - checksum;
-
-        packetLength[i] = packetSize;
-    }
-    return i; //The number of packets in the set
 }
 
 // Send a telemetry block
-
 int sendTelemetryBlock(struct telem_block *telem) {
-    unsigned char apiString[MAX_PACKET_SIZE + PACKET_HEADER_LENGTH + API_HEADER_LENGTH];
-    unsigned char **packets;
-    unsigned char packetLength[20];
 
-    //Allocate array space 20xDEFAULT_SEND_LIMIT matrix
-    packets = malloc(20 * sizeof *packets);
-    int i = 0;
-    for (i = 0; i < 20; i++) {
-        packets[i] = malloc((DEFAULT_SEND_LIMIT + API_HEADER_LENGTH + 3) * sizeof *packets[i]);
-    }
+    unsigned char apiHeader[API_HEADER_LENGTH];
+    unsigned int apiHeaderLength = generateApiHeader(apiHeader, 0);
+    unsigned int packetLength = API_HEADER_LENGTH + sizeof(struct telem_block) + 1; 
+    // Treat the telemetry block as a character array.
+    unsigned char *telemAsArray = telem;
+    unsigned char debugTelemArray[sizeof(struct telem_block)];
 
-    //Generate the shit
-    unsigned int apiLength = generateApiString(apiString, telem);
-    unsigned int numPackets = createPackets(apiLength, apiString, packets, packetLength);
-    for (i = 0; i < numPackets; i++) {
-        // Wait for data link to clear from previous send
-        int j = 0;
-        for (j = 0; j < packetLength[i]; j++) {
-            while (U2STAbits.TRMT == 0);
-            U2TXREG = packets[i][j];
+    unsigned int lengthCheck = 0;
+    unsigned char checksum = 0;
+    unsigned int i;
+    // Send the header
+    for (i = 0; i < API_HEADER_LENGTH; i++ ) {
+        // Culmulative checksum
+        if (i >= 3) {
+            checksum += apiHeader[i] & 0xFF;
         }
-
+        // Wait for data link to clear from previous send
+        while (U2STAbits.TRMT == 0);
+        // Load transmit register
+        U2TXREG = apiHeader[i];
     }
-    destroyTelemetryBlock(telem);
-
-    for (i = 0; i < 20; i++) {
-        free(packets[i]);
+    lengthCheck += i;
+    // Send the telemetry
+    for (i = 0; i < sizeof(struct telem_block); i++) {
+        // Culmulative checksum
+        debugTelemArray[i] = telemAsArray[i] & 0xFF;
+        checksum += telemAsArray[i] & 0xFF;
+        // Wait for data link to clear from previous send
+        while (U2STAbits.TRMT == 0);
+        // Load transmit register
+        U2TXREG = telemAsArray[i];
     }
-    free(packets);
+    lengthCheck += i;
+    // Send the checksum
+    while (U2STAbits.TRMT == 0);
+    U2TXREG = 0xFF - checksum;
+    lengthCheck += 1;
+
     // Note: We send the last piece of data through UART and
     // then forget about it. We assume it will get handled eventually
-     UART1_SendString("Test 3");
+    UART1_SendString("Test 3");
+    if ( packetLength != lengthCheck ) {
+        UART1_SendString("length mismatch");
+    }
     return 0;
 }
