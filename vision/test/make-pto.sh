8759ff9d3d11ea89c44d0d3eaf490cb142cda711
yingted@gmail.com <yingted@gmail.com@0eea08a1-903a-204f-7ded-f54e91f5164d>
2013-12-01 20:38:59 +0000

    improve Makefile
    git-svn-id: https://warg-explorer16-autopilot.googlecode.com/svn/Autopilot.X@75 0eea08a1-903a-204f-7ded-f54e91f5164d

--- vision/test/make-pto.sh
+++ vision/test/make-pto.sh
@@ -2,12 +2,12 @@
 cat << EOF
 # hugin project file
 #hugin_ptoversion 2
-p f0 w256 h114 v176  E0 R1 S0,256,0,448 n"TIFF_m c:LZW r:CROP"
+p f0 w1878 h3375 v171  E0 R1 n"TIFF_m c:LZW r:CROP"
 m g1 i0 f0 m2 p0.00784314
 
 # image lines
 EOF
-./bezier.py path.json | sed 's,n",&output/,'
+./bezier.py "$@"
 cat << EOF
 
 # specify variables that should be optimized
@@ -19,18 +19,18 @@ v
 #hugin_enblendOptions 
 #hugin_enfuseOptions 
 #hugin_hdrmergeOptions -m avg -c
-#hugin_outputLDRBlended true
+#hugin_outputLDRBlended false
 #hugin_outputLDRLayers false
 #hugin_outputLDRExposureRemapped false
 #hugin_outputLDRExposureLayers false
 #hugin_outputLDRExposureBlended false
 #hugin_outputLDRStacks false
 #hugin_outputLDRExposureLayersFused false
-#hugin_outputHDRBlended false
-#hugin_outputHDRLayers true
+#hugin_outputHDRBlended true
+#hugin_outputHDRLayers false
 #hugin_outputHDRStacks false
 #hugin_outputLayersCompression LZW
-#hugin_outputImageType tif
+#hugin_outputImageType png
 #hugin_outputImageTypeCompression LZW
 #hugin_outputJPEGQuality 90
 #hugin_outputImageTypeHDR exr
