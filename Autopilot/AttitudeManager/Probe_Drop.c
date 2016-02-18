d6f60c39686e3190bd625e8292e6701ac5c070f3
UWARG <uw.warg@gmail.com>
2016-02-17 21:37:29 -0500

    Probe Drop working from remote commands.

---
+++ Autopilot/AttitudeManager/Probe_Drop.c
@@ -0,0 +1,29 @@
+/*
+ * File:   StateMachine.c
+ * Author: Jim Lin
+ *
+ * Created on February 9, 2016, 9:00 PM
+ * range from -1024 ->1024
+ *
+ */
+
+#include "StateMachine.h"
+#include "../Common/debug.h"
+#include "Probe_Drop.h"
+
+int prob_drop[2] = {-240, -230};
+// prob_drop_old(channle 6) close -240  open 500
+// prob_drop_new(channle 7) close -230  open 510
+
+//char dropping = 0;
+//int prob_old_state = 0
+void dropProbe(char num)
+{
+    prob_drop[num - 1] = 510;
+}
+
+int probeStatus(char num)
+{
+    return prob_drop[num - 1];
+}
+//char getProbeDropState(){
---
+++ Autopilot/AttitudeManager/Probe_Drop.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   Probe_Drop.h
+ * Author: WARG
+ *
+ * Created on February 17, 2016, 8:52 PM
+ */
+
+#ifndef PROBE_DROP_H
+#define	PROBE_DROP_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define PROBE1 1
+#define PROBE2 2
+
+void dropProbe(char num);
+int probeStatus(char num);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PROBE_DROP_H */
+
