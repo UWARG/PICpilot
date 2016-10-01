8435e8f2359090edccf14a43d3092e516eecc36b
Serj Babayan <serj.b.babayan@gmail.com>
2016-10-01 12:07:57 -0400

    More comments and some fixes

--- Autopilot/AttitudeManager/UART2.h
+++ Autopilot/AttitudeManager/UART2.h
@@ -6,9 +6,13 @@
  * @copyright Waterloo Aerial Robotics Group 2016 \n
  *   https://raw.githubusercontent.com/UWARG/PICpilot/master/LICENCE 
  */
+
 #pragma once
 
 #include "main.h"
 
 /** Initializes the UART2 connection **/
 void InitUART2();
+
+/** UART2 interrupt function routine for when data comes in from the XBEE **/
+void __attribute__((interrupt, no_auto_psv)) _U2RXInterrupt( void );
