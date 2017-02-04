3d4babb7e35968ade7c5c599c4258a709dd5d8fd
Serj Babayan <serj.b.babayan@gmail.com>
2017-02-04 15:15:59 -0500

    Documented input capture functions

--- Autopilot/AttitudeManager/InputCapture.h
+++ Autopilot/AttitudeManager/InputCapture.h
@@ -1,29 +1,34 @@
 /*
- * File:   InputCapture.h
- * Author: Chris Hajduk
- *
- * Created on March 4, 2013, 10:31 PM
+ * @file InputCapture.h
+ * @author Chris Hajduk
+ * @created March 4, 2013, 10:31 PM
+ * @description This file provides the methods necessary to access the input capture
+ * capabilities of the chip. In essence, it lets you get the PWM values from the 8
+ * PWM capable channels that the chip has.
  */
 
 #ifndef INPUTCAPTURE_H
 #define	INPUTCAPTURE_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-// Includes
 #include "main.h"
-//TODO: Finish these function prototype descriptions
-//Function Prototypes
-void initInputCapture(char initIC);
+
+/**
+ * Initializes capture configuration of the PWM input channels.
+ * @param initIC An 8-bit bitmask specifying which channels to enable (will enable interrupts on these)
+ */
 void initIC(char initIC);
-void initTimer2();
+
+/**
+ * Gets the input capture value (aka PWM) of all the channels
+ * @return Array containing all the channel values
+ */
 unsigned int* getICValues();
+
+/**
+ * Gets the input capture value of a specific value
+ * @param channel number from 0-7
+ * @return PWM value of the channel in ms
+ */
 unsigned int getICValue(unsigned char channel);
 
-#ifdef	__cplusplus
-}
 #endif
-
-#endif	/* INPUTCAPTURE_H */
