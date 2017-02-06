59f1c5a74b586ae90f3ee89266c12a81eb9787c6
Serge Babayan <serj.b.babayan@gmail.com>
2017-02-06 02:01:23 -0500

    Updated documentation

--- Autopilot/Relay/nbproject/Makefile-local-default.mk
+++ Autopilot/Relay/nbproject/Makefile-local-default.mk
@@ -15,23 +15,23 @@
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
 SHELL=cmd.exe
-PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/v3.30/mplab_ide/mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
+PATH:=C:/Program Files (x86)/Microchip/MPLABX/v3.30/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\sys\java\jre1.6.0_32-windows-x64\java-windows/bin/"
+MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\v3.30\sys\java\jre1.8.0_65/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="C:\Program Files (x86)\Microchip\xc16\v1.10\bin\xc16-gcc.exe"
+MP_CC="C:\Program Files (x86)\Microchip\xc16\v1.26\bin\xc16-gcc.exe"
 # MP_CPPC is not defined
 # MP_BC is not defined
-# MP_AS is not defined
-# MP_LD is not defined
-MP_AR="C:\Program Files (x86)\Microchip\xc16\v1.10\bin\xc16-ar.exe"
-DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar" 
-MP_CC_DIR="C:\Program Files (x86)\Microchip\xc16\v1.10\bin"
+MP_AS="C:\Program Files (x86)\Microchip\xc16\v1.26\bin\xc16-as.exe"
+MP_LD="C:\Program Files (x86)\Microchip\xc16\v1.26\bin\xc16-ld.exe"
+MP_AR="C:\Program Files (x86)\Microchip\xc16\v1.26\bin\xc16-ar.exe"
+DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/v3.30/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+MP_CC_DIR="C:\Program Files (x86)\Microchip\xc16\v1.26\bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-# MP_AS_DIR is not defined
-# MP_LD_DIR is not defined
-MP_AR_DIR="C:\Program Files (x86)\Microchip\xc16\v1.10\bin"
+MP_AS_DIR="C:\Program Files (x86)\Microchip\xc16\v1.26\bin"
+MP_LD_DIR="C:\Program Files (x86)\Microchip\xc16\v1.26\bin"
+MP_AR_DIR="C:\Program Files (x86)\Microchip\xc16\v1.26\bin"
 # MP_BC_DIR is not defined
