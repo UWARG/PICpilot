efc8ae9f1dd2e20731e00cd4385868ab68d491f8
Serj Babayan <serj.b.babayan@gmail.com>
2017-03-04 23:03:44 -0500

    Fixed logger unit test

--- Autopilot/AttitudeManager/test/test/test_logger.c
+++ Autopilot/AttitudeManager/test/test/test_logger.c
@@ -64,36 +64,36 @@ void test_initLoggerShouldCallInitUART(void)
 
 void test_debugShouldCallQueueUartWithCorrectMessage(void){
     getTXSpace_ExpectAndReturn(LOGGER_UART_INTERFACE,100);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)DEBUG_TAG_STRING, DEBUG_TAG_STRING_LENGTH, DEBUG_TAG_STRING_LENGTH);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)DEBUG_TAG_STRING, DEBUG_TAG_STRING_LENGTH, DEBUG_TAG_STRING_LENGTH);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
 
     debug("hello");
 }
 
 void test_warningShouldCallQueueUartWithCorrectMessage(void){
     getTXSpace_ExpectAndReturn(LOGGER_UART_INTERFACE,100);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)WARNING_TAG_STRING, WARNING_TAG_STRING_LENGTH, WARNING_TAG_STRING_LENGTH);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)WARNING_TAG_STRING, WARNING_TAG_STRING_LENGTH, WARNING_TAG_STRING_LENGTH);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
 
     warning("hello");
 }
 
 void test_errorShouldCallQueueUartWithCorrectMessage(void){
     getTXSpace_ExpectAndReturn(LOGGER_UART_INTERFACE,100);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)ERROR_TAG_STRING, ERROR_TAG_STRING_LENGTH,ERROR_TAG_STRING_LENGTH);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)ERROR_TAG_STRING, ERROR_TAG_STRING_LENGTH,ERROR_TAG_STRING_LENGTH);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"hello", 5, 5);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3);
 
     error("hello");
 }
 
 void test_writeMessageWithOneCharacterShouldCallQueueWithCorrectMessage(void){
     getTXSpace_ExpectAndReturn(LOGGER_UART_INTERFACE,100);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)ERROR_TAG_STRING, ERROR_TAG_STRING_LENGTH,ERROR_TAG_STRING_LENGTH);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"r", 1, 1);
-    quoueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3); 
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)ERROR_TAG_STRING, ERROR_TAG_STRING_LENGTH,ERROR_TAG_STRING_LENGTH);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"r", 1, 1);
+    queueTXData_ExpectWithArray(LOGGER_UART_INTERFACE, (unsigned char*)"\r\n\0", 3, 3); 
 
     error("r");
 }
