f86028171eaea881290f61bc6ae325eac3181205
Serj Babayan <serj.b.babayan@gmail.com>
2017-03-09 09:56:51 -0500

    Added method to get rx buffer size of uart interface

--- Autopilot/AttitudeManager/Drivers/Radio.h
+++ Autopilot/AttitudeManager/Drivers/Radio.h
@@ -13,6 +13,9 @@
 #ifndef RADIO_H
 #define	RADIO_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #define RADIO_XBEE 0
 #define RADIO_3DR 1
 
@@ -42,15 +45,26 @@ void queueRadioDownlinkPacket();
  * @param data_length Length of the aforementioned data
  * @return 1 if the downlink data was successfully queued. 0 otherwise (probably because of malloc)
  */
-char queueDownlinkPacket(unsigned char* data, unsigned int data_length);
+bool queueDownlinkPacket(unsigned char* data, unsigned int data_length);
 
 /**
  * Sends the next queued up down the data link. Note that this will at most only 
  * send 1 packet down
- * @return 1 if something was sent down, 0 otherwise. This may be 0 if there is no
+ * @return true if something was sent down, false otherwise. This may be false if there is no
  * more data to send, or if the UART buffers are currently too full
  */
-char sendQueuedDownlinkPacket();
+bool sendQueuedDownlinkPacket();
+
+/**
+ * Parses a received uplink packet received from the radio
+ * @param length Length of the returned array, if applicable
+ * @return A pointer to the parsed array of data, if it exists. This array will be
+ *      created dynamically, so free it afterwords. This may also be NULL. A null 
+ *      would indicate that 1) The UART RX buffer hasn't filled and the radio hasn't 
+ *      finished its RX tranmission, 2) The parsed uplink was in some way invalid, 
+ *      3) The packet is not applicable to received data from the link (ie. AT command)
+ */
+uint8_t* parseUplinkPacket(uint16_t* length);
 
 #endif
 
