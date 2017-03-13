46cc4e3a4a41abe4e7b61540426a762489d2cfb8
Ian Frosst <ianjfrosst@gmail.com>
2017-03-13 02:03:32 -0400

    Started re-writing SPI

--- Autopilot/AttitudeManager/VN100.c
+++ Autopilot/AttitudeManager/VN100.c
@@ -18,6 +18,7 @@
 #include "VN100.h"
 #include "VN_lib.h"
 #include "main.h"
+#include "../Common/Interfaces/SPI.h"
 
 
 #ifdef _VN100
@@ -52,39 +53,7 @@ VN100_SPI_Packet VN_SPI_LastReceivedPacket = {0, 0, 0, 0, {{0}}};
 * Return         : None
 *******************************************************************************/
 void VN100_initSPI(){
-        //Set interrupts
-    IFS2bits.SPI2IF = 0; //Clear interrupt flag
-    IEC2bits.SPI2IE = 0; //Disable interrupt (so it doesnt mess with this initialization)
-    //Continue module operation in idle mode
-    //SPI2STATbits.SPISIDL = 0; commented out as it does not exist in Trevor's past code
-    //SPI clock controlled by this module
-    SPI2CON1bits.DISSCK = 0;
-    //Output pins are controlled by this module
-    SPI2CON1bits.DISSDO = 0;
-    //16/8 bit communication mode (1/0)
-    SPI2CON1bits.MODE16 = 0; //8
-    //Master mode(1)/Slave mode(0)
-    SPI2CON1bits.MSTEN = 1; //MASTER
-    //Sample Phase (end/middle)
-    SPI2CON1bits.SMP = 0; //Sample the input at the middle of the square wave
-    //Clock Edge Select
-    SPI2CON1bits.CKE = 0; //Output data changes from idle state to active clock state (1 is the opposite)
-    //Clock Polarity
-    SPI2CON1bits.CKP = 1; //Idle clock state is high, active clock state is low
-    //Secondary Prescale (The prescale of the prescale)(3 bits)
-    SPI2CON1bits.SPRE = 0b110; //8:1 prescale
-    //Primart Prescale (The prescale of the clock) (2 bits)
-    SPI2CON1bits.PPRE = 0b10; //64:1 prescale
-
-    //Then enable interrupts
-    IFS2bits.SPI2IF = 0; //Clear interrupt flag
-    IEC2bits.SPI2IE = 0; //Disable interrupt
-
-    SPI2STATbits.SPIROV = 1;
-    //Enable SPI
-    SPI2STATbits.SPIEN = 1;
-
-    TRISGbits.TRISG9 = 0;
+    initSPI(2, 0, 1);
 }
 /*******************************************************************************
 * Function Name  : VN100_SPI_ReadRegister(unsigned char sensorID, unsigned char regID, unsigned char regWidth)
--- Autopilot/Common/Interfaces/SPI.c
+++ Autopilot/Common/Interfaces/SPI.c
@@ -1,61 +1,119 @@
+/**
+ * @file SPI.h
+ * @author Ian Frosst
+ * @date March 13, 2017
+ * @copyright Waterloo Aerial Robotics Group 2017 \n
+ *   https://raw.githubusercontent.com/UWARG/PICpilot/master/LICENCE 
+ */
+
 #include <p33FJ256GP710.h>
-#include <SPI.h>
+#include "SPI.h"
+#include "../clock.h"
+
+#include <stdint.h>
+
 
 /*
- * File:   SPI.c
- * Author: Chris Hajduk
- *
- * Created on July 16, 2013, 7:23 PM
+ * Use this file as a modular way to interface with a device over SPI.
  */
 
-/*
- * Use this file as a modular way to interface the VectorNav for common IMU functions.
- */
+typedef enum {
+    PIN_LOW = 0,
+    PIN_HIGH
+} pin_state;
 
 /*
  * SPI uses 4 pins:
- * SDIx: Serial Data Input              --> RF7
- * SDOx: Serial Data Output             --> RF8
- * SCKx: Shift Clock Input/Output       --> RF6
- * SSx/FSYNCx: Active-Low slave select or Frame Synchronization --> RB2
- *
+ * SDIx: Serial Data Input              --> 1 : RF7, 2 : RG7
+ * SDOx: Serial Data Output             --> 1 : RF8, 2 : RG8
+ * SCKx: Shift Clock Input/Output       --> 1 : RF6, 2 : RG6
+ * SSx/FSYNCx: Slave select/Frame Sync  --> 1 : RB2, 2 : RG9
+ * Slave select is active-low
  */
 
-void InitSPI(){
-    //Set interrupts
-    IFS0bits.SPI1IF = 0; //Clear interrupt flag
-    IEC0bits.SPI1IE = 0; //Disable interrupt (so it doesnt mess with this initialization)
+void SPI_SetSS(uint8_t interface, pin_state state) {
+    if (interface == 1) {
+        PORTBbits.RB2 = state;
+    } else if (interface == 2) {
+        PORTGbits.RG9 = state;
+    }
+}
 
+void initSPI(uint8_t interface, uint16_t clock, uint8_t master){
+    
+    if (interface == 1) {
 
-    //Continue module operation in idle mode
-    SPI1STATbits.SPISIDL = 1;
-    //SPI clock controlled by this module
-    SPI1CON1bits.DISSCK = 0;
-    //Output pins are controlled by this module
-    SPI1CON1bits.DISSDO = 0;
-    //16/8 bit communication mode (1/0)
-    SPI1CON1bits.MODE16 = 0; //8
-    //Master mode(1)/Slave mode(0)
-    SPI1CON1bits.MSTEN = 1; //MASTER
-    //Sample Phase (end/middle)
-    SPI1CON1bits.SMP = 0; //Sample the input at the end of the square wave
-    //Clock Edge Select
-    SPI1CON1bits.CKE = 0; //Output data changes from idle state to active clock state (1 is the opposite)
-    //Clock Polarity
-    SPI1CON1bits.CKP = 0; //Idle clock state is low, active clock state is high
-    //Secondary Prescale (The prescale of the prescale)(3 bits)
-    SPI1CON1bits.SPRE = 0; //8:1 prescale
-    //Primart Prescale (The prescale of the clock) (2 bits)
-    SPI1CON1bits.PPRE = 0; //64:1 prescale
+        //Set interrupts
+        IFS0bits.SPI1IF = 0; //Clear interrupt flag
+        IEC0bits.SPI1IE = 0; //Disable interrupt (so it doesnt mess with this initialization)
 
-    //Then enable interrupts
-    IFS0bits.SPI1IF = 0; //Clear interrupt flag
-    IEC0bits.SPI1IE = 1; //Enable interrupt
+        //Continue module operation in idle mode
+        SPI1STATbits.SPISIDL = 0;
+        //SPI clock controlled by this module
+        SPI1CON1bits.DISSCK = 0;
+        //Output pins are controlled by this module
+        SPI1CON1bits.DISSDO = 0;
+        //16/8 bit communication mode (1/0)
+        SPI1CON1bits.MODE16 = 0; //8
+        //Master mode(1)/Slave mode(0)
+        SPI1CON1bits.MSTEN = master; //MASTER
+        //Sample Phase (end/middle)
+        SPI1CON1bits.SMP = 0; //Sample the input at the end of the square wave
+        //Clock Edge Select
+        SPI1CON1bits.CKE = 0; //Output data changes from idle state to active clock state (1 is the opposite)
+        //Clock Polarity
+        SPI1CON1bits.CKP = 0; //Idle clock state is low, active clock state is high
+        //Secondary Prescale (The prescale of the prescale)(3 bits)
+        SPI1CON1bits.SPRE = 0b000; //8:1 prescale
+        //Primart Prescale (The prescale of the clock) (2 bits)
+        SPI1CON1bits.PPRE = 0b00; //64:1 prescale
 
-    //Enable SPI
-    SPI1STATbits.SPIEN = 1;
+        //Then enable interrupts
+        IFS0bits.SPI1IF = 0; //Clear interrupt flag
+        IEC0bits.SPI1IE = 1; //Enable interrupt
 
-    //Then write to the SPI1BUF
+        //Enable SPI
+        SPI1STATbits.SPIEN = 1;
+        
+        TRISBbits.TRISB2 = !master;
+
+        //Then write to the SPI1BUF
+    } else if (interface == 2) {
+        
+        SPI2STATbits.SPIEN = 0; // Disable for configuration
+        SPI2STATbits.SPISIDL = 0; //Continue module operation in idle mode
+
+        IFS2bits.SPI2IF = 0; //Clear interrupt flag
+        IEC2bits.SPI2IE = 0; //Disable interrupt (so it doesn't mess with initialization)
+    
+        SPI2CON1bits.DISSCK = 0; //SPI clock controlled by this module
+        SPI2CON1bits.DISSDO = 0; //Output pins are controlled by this module
+        
+        SPI2CON1bits.MODE16 = 0; //16/8 bit communication mode (1/0)
+        
+        SPI2CON1bits.MSTEN = master; // set master or slave mode
+        //Sample Phase (end/middle)
+        SPI2CON1bits.SMP = 0; //Sample the input at the middle of the square wave
+        //Clock Edge Select
+        SPI2CON1bits.CKE = 0; //Output data changes from idle state to active clock state (1 is the opposite)
+        //Clock Polarity
+        SPI2CON1bits.CKP = 1; //Idle clock state is high, active clock state is low
+        //Secondary Prescale (The prescale of the prescale)(3 bits)
+        SPI2CON1bits.SPRE = 0b110; //2:1 prescale
+        //Primary Prescale (The prescale of the clock) (2 bits)
+        SPI2CON1bits.PPRE = 0b10; //4:1 prescale
+
+        //Then enable interrupts
+        IFS2bits.SPI2IF = 0; //Clear interrupt flag
+        IEC2bits.SPI2IE = 0; //Disable interrupt
+
+        SPI2STATbits.SPIROV = 1;
+        //Enable SPI
+        SPI2STATbits.SPIEN = 1;
+
+        
+        TRISGbits.TRISG9 = !master;
+    }
 
 }
 void sendData(char command){
