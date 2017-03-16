fd998cd16de31ae4b0d5f3c9f8586b291aa15fb3
Ian Frosst <ianjfrosst@gmail.com>
2017-03-16 14:37:03 -0400

    Added IMU driver outline

---
+++ Autopilot/AttitudeManager/IMU.h
@@ -0,0 +1,29 @@
+/**
+ * @file IMU.h
+ * @author Ian Frosst
+ * @date MArch 16, 2017
+ * @copyright Waterloo Aerial Robotics Group 2017 \n
+ *   https://raw.githubusercontent.com/UWARG/PICpilot/master/LICENCE
+ */
+
+#ifndef IMU_H
+#define	IMU_H
+
+#define VN100 0
+#define MPU9250 1
+#define MPU6050 2
+
+#define USE_IMU VN100
+
+typedef struct IMU_data {
+    float roll, pitch, heading;
+    float rollRate, pitchRate, yawRate;
+} IMU_data;
+
+
+void initIMU();
+
+void updateIMU();
+
+#endif	/* IMU_H */
+
