40397c66a65f94a8f67da81751e67b0534b3af8c
Serj Babayan <serj.b.babayan@gmail.com>
2017-04-01 13:24:16 -0400

    Displaying 0 on uhf link quality if its disconnected

--- Autopilot/AttitudeManager/Peripherals/UHF.c
+++ Autopilot/AttitudeManager/Peripherals/UHF.c
@@ -8,6 +8,9 @@
 #include "UHF.h"
 
 uint8_t getUHFRSSI(uint32_t sys_time){
+    if (getPWMInputStatus() & UHF_RSSI_CHANNEL == 1){
+        return 0;
+    }
     uint16_t unscaled = getICValues(sys_time)[UHF_RSSI_CHANNEL - 1];
     int16_t scaled = (((unscaled - LOWER_PWM)*100)/(UPPER_PWM - LOWER_PWM));
     if (scaled > 100){
@@ -19,6 +22,9 @@ uint8_t getUHFRSSI(uint32_t sys_time){
 }
 
 uint8_t getUHFLinkQuality(uint32_t sys_time){
+    if (getPWMInputStatus() & UHF_LINK_QUALITY_CHANNEL == 1){
+        return 0;
+    }
     uint16_t unscaled = getICValues(sys_time)[UHF_LINK_QUALITY_CHANNEL - 1];
     int16_t scaled = (((unscaled - LOWER_PWM)*100)/(UPPER_PWM - LOWER_PWM));
     if (scaled > 100){
