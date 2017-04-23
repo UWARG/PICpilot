901644572b898da304023e08034a9c91d126eb6f
Serge Babayan <serj.b.babayan@gmail.com>
2017-04-23 05:20:34 -0400

    Initial version of new project

---
+++ Autopilot/GPS/SPI.h
@@ -0,0 +1,15 @@
+/**
+ * @file SPI.h
+ * @author Serge Babayan
+ * @date February 14, 2017, 5:08AM
+ * @copyright Waterloo Aerial Robotics Group 2017 \n
+ *   https://raw.githubusercontent.com/UWARG/PICpilot/master/LICENCE
+ */
+
+#ifndef SPI_H
+#define	SPI_H
+
+void initSPI(void);
+
+#endif
+
