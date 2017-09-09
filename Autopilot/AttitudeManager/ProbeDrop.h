0dd057f456bf17da3900d0ca610890de48558e35
jpaduchowski <31752282+jpaduchowski@users.noreply.github.com>
2017-09-09 16:42:45 -0400

    Update ProbeDrop.h

--- Autopilot/AttitudeManager/ProbeDrop.h
+++ Autopilot/AttitudeManager/ProbeDrop.h
@@ -4,6 +4,11 @@
 * @created September 9, 2017
 */
 
+#ifndef ProbeDrop_H
+#define ProbeDrop_H
+
 void servoStartup(); //ensures all servos are closed on startup
 
 void probeDrop(int probeNum, int openState); //probeNum selects which servo to use, openState sets state ( open = 1, closed = 0)
+
+#endif //ProbeDrop_H
